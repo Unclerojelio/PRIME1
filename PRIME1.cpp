7acadef492eb5cc600de1b68581853759b3e1848
Roger Banks <roger_banks@mac.com>
2015-09-26 11:33:16 -0500

    Stub of sieve function added

--- PRIME1.cpp
+++ PRIME1.cpp
@@ -39,19 +39,50 @@
 // After cluster change, please consider PRINT as a more challenging problem.
 
 #include <iostream>
-#include <map>
 #include <vector>
 #include <climits>
 
 using namespace std;
 
+std::vector<bool> sieve_of_eratosthenes( unsigned long int const n_length )
+{
+    std::vector<bool> primeList(n_length);
+    primeList[0] = 1;
+    primeList[1] = 1;
+    for(unsigned long int i = 2; i <= 31622; i++) {
+    	primeList[i] = 1;
+    }
+    return primeList;
+}
+
 int main(void)
 {
+	unsigned long int MAX = ULONG_MAX;
+	std::vector<bool> const primeList = sieve_of_eratosthenes( 1000000000 );
 	int NumberOfTests = 0;
+	int upper, lower;
 	cin >> NumberOfTests;
 	while(NumberOfTests--)
 	{
-
 		//Read in in two integers and find all the primes between them. 
+		cin >> lower >> upper;
+		cout << lower << '\t' << upper << endl;
+		// sqrt(1000000000) = 31622
+		// create an boolean array [0,1000000000]initial to all true;
+		// set 0,1 to false;
+		
+		// Input: an integer n > 1
+ 
+		// Let A be an array of Boolean values, indexed by integers 2 to n,
+		// initially all set to true.
+		//  
+		//  for i = 2, 3, 4, ..., not exceeding √n:
+		//   if A[i] is true:
+		//     for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n :
+		//       A[j] := false
+		//  
+		// Output: all i such that A[i] is true.
+
 	}
+	cout << MAX << endl;
 }
